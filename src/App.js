8711ad9d4f46c163b3f9c2bc6ecc437310216724
Abhishek Anand <anand2000ys@gmail.com>
2023-03-27 22:32:34 +0530

    api added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
 
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   const handleChange = (e) => {
     setSearch(e.target.value);
   };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div className="coin-app">
